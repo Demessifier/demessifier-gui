4c35e2cd2812e42b52269d8a9ee0d1f25c65a0f0
McLayn <mclayn.v@gmail.com>
2023-11-11 20:13:49 +0100

    vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ rollupTypes: true })],
   build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["cjs", "es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      input: {
+        index: "src/index.ts",
+      },
+      // What shouldn't be bundled into the library: dependencies, tests
+      external: [
+        "vue",
+        "@vueuse/core",
+        new RegExp(".*.test.ts"),
+        new RegExp(".*.cy.ts"),
+        "/test",
+      ],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
     outDir: "build",
-    target: "esnext",
+    // Reduce bloat from legacy polyfills.
+    target: "ESNext",
+    // Leave minification up to applications.
+    minify: false,
   },
   css: {
     preprocessorOptions: {
