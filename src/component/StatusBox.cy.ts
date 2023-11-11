68200291b8512804f55d895623f5bed4477e40ab
McLayn <mclayn.v@gmail.com>
2023-11-11 17:23:43 +0100

    tests

--- src/component/StatusBox.cy.ts
+++ src/component/StatusBox.cy.ts
@@ -6,30 +6,37 @@ function test(
   headerText: string,
   expectMinimized: boolean
 ) {
-  cy.get("div.status-box")
-    .should("have.css", "color", "rgb(255, 255, 255)")
-    .should("have.css", "background-color", "rgb(0, 0, 139)")
-    .should(
-      "have.attr",
+  cy.get("div.status-box").should((statusBox) => {
+    expect(statusBox).to.have.css("color", "rgb(255, 255, 255)");
+    expect(statusBox).to.have.css("background-color", "rgb(0, 0, 139)");
+    expect(statusBox).to.have.attr(
       "title",
       expectMinimized ? `Show: \n${headerText}` : ""
     );
+  });
   if (expectMinimized) {
-    cy.get("div.status-box div").should("have.length", 0);
-    cy.get("h3 span").should("have.length", 0);
+    cy.get("div.status-box div").should((statusBoxDiv) => {
+      expect(statusBoxDiv).to.not.exist;
+    });
+    cy.get("h3 span").should((h3Span) => {
+      expect(h3Span).to.not.exist;
+    });
   } else {
-    cy.get("div.status-box div")
-      .should("have.length", 1)
-      .should("contain.text", testingBodyText);
-    cy.get("h3 span")
-      .should("have.length", 1)
-      .should("contain.text", headerText);
+    cy.get("div.status-box div").should((statusBoxDiv) => {
+      expect(statusBoxDiv).to.have.length(1);
+      expect(statusBoxDiv).to.contain.text(testingBodyText);
+    });
+    cy.get("h3 span").should((h3Span) => {
+      expect(h3Span).to.have.length(1);
+      expect(h3Span).to.contain.text(headerText);
+    });
   }
-  cy.get("h3").should(
-    "have.attr",
-    "title",
-    expectMinimized ? `Show: \n${headerText}` : "Minimize"
-  );
+  cy.get("h3").should((h3) => {
+    expect(h3).to.have.attr(
+      "title",
+      expectMinimized ? `Show: \n${headerText}` : "Minimize"
+    );
+  });
 }
 
 describe("<StatusBox />", () => {
