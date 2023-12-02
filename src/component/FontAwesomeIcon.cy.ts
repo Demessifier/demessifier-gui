15bdf0d5895c6f287bb519bcb8f7f1a2d626a581
McLayn <mclayn.v@gmail.com>
2023-12-02 17:20:29 +0100

    refactor tests

--- src/component/FontAwesomeIcon.cy.ts
+++ src/component/FontAwesomeIcon.cy.ts
@@ -3,12 +3,11 @@ import { FaIcon, faIconInit } from "../provider/fa-icon";
 
 const iconExamples: FaIcon[] = ["fas-bars", "fas-chevron-down"];
 
-describe("<FaIconWrapper />", () => {
+describe("FaIconWrapper component", () => {
   faIconInit();
   for (const icon of iconExamples) {
     const title = `Testing title of ${icon} icon`;
-    it(`renders ${icon}`, () => {
-      // see: https://test-utils.vuejs.org/guide/
+    it(`Renders ${icon}`, () => {
       cy.mount(FaIconWrapper, { props: { icon: icon, title: title } });
       cy.get("svg").should((svg) => {
         expect(svg).to.have.length(1);
@@ -17,8 +16,7 @@ describe("<FaIconWrapper />", () => {
         expect(svgTitle).to.contain.text(title);
       });
     });
-    it(`spins ${icon}`, () => {
-      // see: https://test-utils.vuejs.org/guide/
+    it(`Spins ${icon}`, () => {
       cy.mount(FaIconWrapper, {
         props: { icon: icon, title: title, spin: true },
       });
