a5437468ccf919f397b5d821de83fc9472c67a74
McLayn <mclayn.v@gmail.com>
2023-12-03 18:05:37 +0100

    fix: building works again

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { faIconInit } from "./provider/fa-icon";
+import FontAwesomeIcon from "./component/FontAwesomeIcon.vue";
 import "./css/default-css-variables.css";
 import "./css/global.css";
 import "./css/table.css";
 import "./css/form.css";
+
+export { FontAwesomeIcon }; // Removing this breaks vite building
 
 const app = createApp(App);
 faIconInit();
