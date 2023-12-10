831adff49c3178507f7d1cbc8b25e70e5cad5da1
McLayn <mclayn.v@gmail.com>
2023-12-10 21:11:05 +0100

    feat: top-right menu, favicon, logo

---
+++ src/component/layout/MenuTopRight.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ColorSchemeSwitch from "../global-controller/ColorSchemeSwitch.vue";
+</script>
+
+<template>
+  <div class="top-right-menu">
+    <ColorSchemeSwitch></ColorSchemeSwitch>
+  </div>
+</template>
