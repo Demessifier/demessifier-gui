ab1409d549b7c1e7c8421ed88427a209f082ef60
Vojtěch Miklín <mclayn.v@gmail.com>
2024-01-31 18:13:43 +0100

    fix: test broken by auto resolve in rebase

--- src/component/StatusBox.cy.ts
+++ src/component/StatusBox.cy.ts
@@ -4,7 +4,12 @@ import {
   getFlavorItem,
   StatusBoxFlavorName,
 } from "../provider/status-box";
-import { getIconClassName } from "../provider/fa-icon";
+import {
+  getColorNameComplementFromPlainColorName,
+  getColorNameFromPlainColorName,
+  getCurrentColor,
+  hexToRgb,
+} from "../provider/color-palette";
 
 function test(
   flavor: StatusBoxFlavorName,
@@ -13,22 +18,36 @@ function test(
   expectMinimized: boolean,
 ) {
   cy.get("div.status-box").should((statusBox) => {
-    expect(statusBox).to.have.css("color", "rgb(255, 255, 255)");
-    expect(statusBox).to.have.css("background-color", "rgb(0, 0, 139)");
     expect(statusBox).to.have.attr(
       "title",
-      expectMinimized ? `Show: \n${headlineText}` : "",
+      expectMinimized ? `Expand: \n${headlineText}` : "",
+    );
+  });
+  cy.get("div.status-box div.header").should((statusBoxHeader) => {
+    expect(statusBoxHeader).to.have.css(
+      "color",
+      hexToRgb(
+        getCurrentColor(getColorNameComplementFromPlainColorName(flavor)),
+      ),
+    );
+    expect(statusBoxHeader).to.have.css(
+      "background-color",
+      hexToRgb(getCurrentColor(getColorNameFromPlainColorName(flavor))),
+    );
+    expect(statusBoxHeader).to.have.attr(
+      "title",
+      expectMinimized ? `Expand: \n${headlineText}` : "Minimize",
     );
   });
   if (expectMinimized) {
-    cy.get("div.status-box div").should((statusBoxDiv) => {
+    cy.get("div.status-box div.content").should((statusBoxDiv) => {
       expect(statusBoxDiv).to.not.exist;
     });
     cy.get("h3 span").should((h3Span) => {
       expect(h3Span).to.not.exist;
     });
   } else {
-    cy.get("div.status-box div").should((statusBoxDiv) => {
+    cy.get("div.status-box div.content").should((statusBoxDiv) => {
       expect(statusBoxDiv).to.have.length(1);
       expect(statusBoxDiv).to.contain.text(testingBodyText);
     });
@@ -37,15 +56,9 @@ function test(
       expect(h3Span).to.contain.text(headlineText);
     });
   }
-  cy.get("h3").should((h3) => {
-    expect(h3).to.have.attr(
-      "title",
-      expectMinimized ? `Show: \n${headlineText}` : "Minimize",
-    );
-  });
   cy.get("svg").should((svg) => {
     expect(svg).to.have.length(1);
-    expect(svg).to.have.class(getIconClassName(getFlavorItem(flavor).icon));
+    expect(svg).to.have.class(`fa-${getFlavorItem(flavor).icon.iconName}`);
   });
 }
 
