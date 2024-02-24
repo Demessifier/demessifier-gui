65a653300dbad7e72b8b0d6ed6d520091acdf52d
McLayn <mclayn.v@gmail.com>
2024-02-24 21:37:16 +0100

    fix: exporting and index files

---
+++ src/component/global-controller/index.ts
@@ -0,0 +1,3 @@
+export { default as ButtonWithIcon } from "./ButtonWithIcon.vue";
+export { default as ColorPalette } from "./ColorPalette.vue";
+export { default as ColorSchemeSwitch } from "./ColorSchemeSwitch.vue";
---
+++ src/component/index.ts
@@ -0,0 +1,5 @@
+import * as globalController from "./global-controller";
+import * as layout from "./layout";
+
+export { layout, globalController };
+export { default as StatusBox } from "./StatusBox.vue";
