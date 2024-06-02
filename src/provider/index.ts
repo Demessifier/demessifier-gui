40f273adb988536d50c278787d9808522473ae5f
McLayn <mclayn.v@gmail.com>
2024-06-02 17:41:48 +0200

    export HTML Element

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,6 +1,7 @@
 import * as colorPalette from "./color-palette";
 import * as colorScheme from "./color-scheme";
 import * as combinations from "./combinations";
+import * as htmlElement from "./html-element";
 import * as htmlMetaTag from "./html-meta-tag";
 import * as menuExample from "./menu-example";
 import * as notification from "./notification";
@@ -12,6 +13,7 @@ export {
   colorPalette,
   colorScheme,
   combinations,
+  htmlElement,
   htmlMetaTag,
   menuExample,
   notification,
