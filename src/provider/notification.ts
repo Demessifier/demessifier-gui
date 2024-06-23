6cede72a80c8501460aec08a8950f27441b478a4
McLayn <mclayn.v@gmail.com>
2024-06-24 01:37:50 +0200

    remove unnecessary type cast

--- src/provider/notification.ts
+++ src/provider/notification.ts
@@ -70,7 +70,7 @@ export const useDemessifierGuiNotificationsList = defineStore({
           notification.remainingTimeSeconds -= stepDurationMs / 1000;
           return;
         }
-        clearInterval(interval as Interval);
+        clearInterval(interval);
         delete this.notificationsList[notificationId];
       }, stepDurationMs);
 
