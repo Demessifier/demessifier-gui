aaac95000fa9bbeb715550c8da3e0b9183496196
McLayn <mclayn.v@gmail.com>
2024-07-09 23:22:21 +0200

    notification area test without `this`

--- src/component/layout/NotificationsArea.cy.ts
+++ src/component/layout/NotificationsArea.cy.ts
@@ -9,8 +9,18 @@ type NotificationInfo = {
 };
 type NotificationRegister = { [key: string]: NotificationInfo };
 
-describe("NotificationsArea component", function () {
-  beforeEach(function () {
+describe("NotificationsArea component", () => {
+  const notificationRegister = {} as NotificationRegister;
+  let demessifierGuiNotificationsList:
+    | ReturnType<typeof useDemessifierGuiNotificationsList>
+    | undefined = undefined;
+
+  function getDemessifierGuiNotificationsList() {
+    if (demessifierGuiNotificationsList) return demessifierGuiNotificationsList;
+    throw new Error("Not defined yet.");
+  }
+
+  beforeEach(() => {
     cy.mount(NotificationsArea, {
       global: {
         plugins: [
@@ -18,130 +28,130 @@ describe("NotificationsArea component", function () {
         ],
       },
     });
-    const demessifierGuiNotificationsList =
-      useDemessifierGuiNotificationsList();
+    demessifierGuiNotificationsList = useDemessifierGuiNotificationsList();
     demessifierGuiNotificationsList.$reset();
-
-    this.notificationRegister = {} as NotificationRegister;
-
-    this.addNotification = function (timeoutSeconds: number | false): string {
-      const id = demessifierGuiNotificationsList.addNewNotification(
-        "success",
-        "headline",
-        "body",
-        timeoutSeconds,
-      );
-      if (timeoutSeconds === false) timeoutSeconds = Infinity;
-      const endTimeMillis = Date.now() + timeoutSeconds * 1000;
-      this.notificationRegister[id] = { id, timeoutSeconds, endTimeMillis };
-      return id;
-    };
-
-    this.removeNotification = function (id: string) {
-      const demessifierGuiNotificationsList =
-        useDemessifierGuiNotificationsList();
-      demessifierGuiNotificationsList.removeNotification(id, true);
-      this.notificationRegister[id].endTimeMillis = 0;
-    };
-
-    this.testNotifications = function () {
-      const gracePeriodMillis = 500; // increase if tests are running on a slow machine
-      const demessifierGuiNotificationsList =
-        useDemessifierGuiNotificationsList();
-      cy.then(() => {
-        const now = Date.now();
-        const shouldExist = (
-          Object.values(this.notificationRegister) as NotificationInfo[]
-        ).filter((n) => n.endTimeMillis > now);
-        const mightExist = shouldExist.filter(
-          (n) => n.endTimeMillis > now + gracePeriodMillis,
-        );
-        const lengthOfPinia = Object.keys(
-          demessifierGuiNotificationsList.notificationsList,
-        ).length;
-        expect(
-          lengthOfPinia,
-          "pinia store " +
-            Object.keys(demessifierGuiNotificationsList.notificationsList),
-        ).to.be.at.least(mightExist.length);
-        expect(
-          lengthOfPinia,
-          "pinia store " +
-            Object.keys(demessifierGuiNotificationsList.notificationsList),
-        ).to.be.at.most(shouldExist.length);
-        cy.get("div#notifications-backdrop > *").should((notifications) => {
-          expect(notifications.length, "gui component").to.be.at.least(
-            mightExist.length,
-          );
-        });
-        cy.get("div#notifications-backdrop > *").should((notifications) => {
-          expect(notifications.length, "gui component").to.be.at.most(
-            shouldExist.length,
-          );
-        });
-      });
-    };
+    for (let prop in notificationRegister) {
+      if (notificationRegister.hasOwnProperty(prop)) {
+        delete notificationRegister[prop];
+      }
+    }
   });
 
+  function addNotification(timeoutSeconds: number | false): string {
+    const id = getDemessifierGuiNotificationsList().addNewNotification(
+      "success",
+      "headline",
+      "body",
+      timeoutSeconds,
+    );
+    if (timeoutSeconds === false) timeoutSeconds = Infinity;
+    const endTimeMillis = Date.now() + timeoutSeconds * 1000;
+    notificationRegister[id] = { id, timeoutSeconds, endTimeMillis };
+    return id;
+  }
+
+  function removeNotification(id: string) {
+    getDemessifierGuiNotificationsList().removeNotification(id, true);
+    notificationRegister[id].endTimeMillis = 0;
+  }
+
+  function testNotifications() {
+    const gracePeriodMillis = 500; // increase if tests are running on a slow machine
+    cy.then(() => {
+      const now = Date.now();
+      const shouldExist = (
+        Object.values(notificationRegister) as NotificationInfo[]
+      ).filter((n) => n.endTimeMillis > now);
+      const mightExist = shouldExist.filter(
+        (n) => n.endTimeMillis > now + gracePeriodMillis,
+      );
+      const lengthOfPinia = Object.keys(
+        getDemessifierGuiNotificationsList().notificationsList,
+      ).length;
+      expect(
+        lengthOfPinia,
+        "pinia store " +
+          Object.keys(getDemessifierGuiNotificationsList().notificationsList),
+      ).to.be.at.least(mightExist.length);
+      expect(
+        lengthOfPinia,
+        "pinia store " +
+          Object.keys(getDemessifierGuiNotificationsList().notificationsList),
+      ).to.be.at.most(shouldExist.length);
+      cy.get("div#notifications-backdrop > *").should((notifications) => {
+        expect(notifications.length, "gui component").to.be.at.least(
+          mightExist.length,
+        );
+      });
+      cy.get("div#notifications-backdrop > *").should((notifications) => {
+        expect(notifications.length, "gui component").to.be.at.most(
+          shouldExist.length,
+        );
+      });
+    });
+  }
+
   it("Is empty in the beginning", function () {
-    const demessifierGuiNotificationsList =
-      useDemessifierGuiNotificationsList();
     expect(
-      Object.keys(demessifierGuiNotificationsList.notificationsList),
+      Object.keys(getDemessifierGuiNotificationsList().notificationsList),
       "pinia store - init",
     ).to.have.length(0);
     cy.get("div#notifications-backdrop > *").should((notifications) => {
       expect(notifications, "gui component - init").to.have.length(0);
     });
-    this.testNotifications();
+    testNotifications();
   });
 
   describe("Add notification 1", function () {
     beforeEach(function () {
-      this.addNotification(1);
+      addNotification(1);
     });
 
     it("Renders all added notifications 1", function () {
-      this.testNotifications();
+      testNotifications();
     });
 
     describe("Add notification 2", function () {
       beforeEach(function () {
-        this.addNotification(2);
+        addNotification(2);
       });
 
       it("Renders all added notifications 2", function () {
-        this.testNotifications();
+        testNotifications();
       });
 
       describe("Add notification 3", function () {
         beforeEach(function () {
-          this.addNotification(false);
+          addNotification(false);
         });
 
         it("Renders all added notifications 3", function () {
-          this.testNotifications();
+          testNotifications();
         });
 
         describe("Add notification 4", function () {
+          let idToBeClosed: string = "";
+
           beforeEach(function () {
-            this.idToBeClosed = this.addNotification(2);
+            idToBeClosed = addNotification(2);
           });
 
           it("Renders all added notifications 4", function () {
-            this.testNotifications();
+            testNotifications();
           });
 
           describe("Add notification 5", function () {
+            let idToBeRenewed: string = "";
+
             beforeEach(function () {
-              this.idToBeRenewed = this.addNotification(1);
+              idToBeRenewed = addNotification(1);
               // TODO: mouse over this one and reset its endTimeMillis:
-              // const notification = this.notificationRegister[this.idToBeRenewed];
+              // const notification = notificationRegister[idToBeRenewed];
               // notification.endTimeMillis = Date.now() + notification.timeoutSeconds * 1000;
             });
 
             it("Renders all added notifications 5", function () {
-              this.testNotifications();
+              testNotifications();
             });
 
             describe("Waiting for half of the notifications to disappear", function () {
@@ -157,16 +167,16 @@ describe("NotificationsArea component", function () {
               });
 
               it("Renders all remaining notifications", function () {
-                this.testNotifications();
+                testNotifications();
               });
 
               describe("Remove notification", function () {
                 beforeEach(function () {
-                  this.removeNotification(this.idToBeClosed);
+                  removeNotification(idToBeClosed);
                 });
 
                 it("Renders all remaining notifications", function () {
-                  this.testNotifications();
+                  testNotifications();
                 });
               });
             });
